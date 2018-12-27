c2ff85c6be52eb70f970dcffc1c6655590334914
Ismail Jalloh <ijalloh21@gmail.com>
2018-12-27 12:57:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma (phrase)
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == cphrase
     puts "NO, NOT SINCE 1938!"
-  if phrase =! cphrase
+  elsif phrase =! cphrase
     puts "HUH?! SPEAK UP, SONNY!"
   end
 end
