362b04a65134599a3a5ee7e7bfcdc35ea086ca63
Ismail Jalloh <ijalloh21@gmail.com>
2018-12-27 12:58:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma (phrase)
   cphrase = phrase.upcase 
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == cphrase
     puts "NO, NOT SINCE 1938!"
   elsif phrase =! cphrase
